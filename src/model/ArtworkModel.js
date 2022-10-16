8ba3e5598b9729cd1f131f69f40e434a796fc381
Coding Bugs <coding.bugs@outlook.com>
2022-10-16 17:42:22 +0200

    refactor: change mock objects

--- src/model/ArtworkModel.js
+++ src/model/ArtworkModel.js
@@ -1,5 +1,3 @@
-import { faker } from '@faker-js/faker';
-
 export default function ArtworkModel() {
 
     const build = (data) => {
@@ -16,19 +14,5 @@ export default function ArtworkModel() {
         };
     };
 
-    const mock = () => {
-        return {
-            id: faker.datatype.string(),
-
-            artistName: faker.name.fullName(),
-            department: faker.commerce.department(),
-            isHighlight: faker.datatype.boolean(),
-            image: faker.image.abstract(640, 480),
-            thumbnail: faker.image.abstract(128, 128*4/3),
-            title: faker.datatype.string(),
-            type: faker.datatype.string(),
-        };
-    };
-
-    return {build, mock};
+    return { build };
 }
--- src/services/useMockArtwork.js
+++ src/services/useMockArtwork.js
@@ -1,15 +1,15 @@
-import ArtworkModel from "../model/ArtworkModel";
+import ArtworkMockModel from "../model/ArtworkMockModel";
 
-export default function useArtwork() {
+export default function useMockArtwork() {
 
-    const { mock } = ArtworkModel();
+    const { build } = ArtworkMockModel();
 
     const get = (artworkId) => {
-        return Promise.resolve(mock());
+        return Promise.resolve(build());
     };
 
     const find = (keyword) => {
-        const data = Array(40).fill(0).map(i => mock());
+        const data = Array(40).fill(0).map(i => build());
         return Promise.resolve(data);
     };
 
